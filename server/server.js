cd3502a8df3594318cd80e08f5e2d7ef90e97186
arsene <arsichizy@gmail.com>
2018-07-19 15:48:01 +0200

    We added sockets, to channel the flow of data

--- server/server.js
+++ server/server.js
@@ -7,8 +7,18 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3200;
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('Disconnected from server');
+    });
+});
+
+
 
 server.listen(port, () => {
     console.log(`Server is running on ${port}`);
